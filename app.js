6a3b194bc79c313fe7c38273a598cb4a92f186e3
tranminhkhang8198 <minhkhang811998@gmail.com>
2019-12-19 21:43:04 +0700

    Handling authentication

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport)
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
-
-const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('It works');
 });
 
+// Use Routes
+app.use('/auth', auth);
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://khangtran:YwdYpWrxatIfXtbY@cluster0-jkqto.mongodb.net/storybooks-dev?retryWrites=true&w=majority',
+    googleClientID: '493662453785-maol8opeclb5sgh4g6pqje8cnhvenmrs.apps.googleusercontent.com',
+    googleClientSecret: 'D0NUUMvnpV5cHV7YVzdRp5Y_'
+}
