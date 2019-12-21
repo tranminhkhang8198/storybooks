05ecaf8fe40a68f56ab5c63b8eaa3fa8527236d2
tranminhkhang8198 <minhkhang811998@gmail.com>
2019-12-21 15:55:27 +0700

    Adding session authentication

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+// Load User Model
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport)
@@ -8,15 +13,43 @@ require('./config/passport')(passport)
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+// Mongoose Connect 
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('It works');
 });
 
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
 // Use Routes
 app.use('/auth', auth);
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
